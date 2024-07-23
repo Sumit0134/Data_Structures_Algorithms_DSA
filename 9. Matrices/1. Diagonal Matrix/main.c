90a7f9fcbe486b112a27f98589278921a150837d
Sumit Kumar Shaw <sumitshaw8274060134@gmail.com>
2024-07-23 15:11:48 +0530

    Matrices & Diagonal Matrix

---
+++ 9. Matrices/1. Diagonal Matrix/main.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, m;
+
+    printf("Enter the number of rows n: ");
+    scanf("%d", &n);
+
+    printf("Enter the number of columns m: ");
+    scanf("%d", &m);
+
+    int matrix[n][m], A[n];
+
+    if(n!=m)
+    {
+        printf("As the number of rows is not equal to the number of columns, we cannot create a diagonal matrix.");
+        return 0;
+    }
+    else
+    {
+        printf("Enter total of 5 diagonal elements for a diagonal matrix: ");
+        for(int i=0; i<n; i++)
+        {
+            scanf("%d", &A[i]);
+        }
+    }
+
+    printf("The diagonal matrix is:\n");
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(i==j)
+            {
+                printf("%d ", A[i]);
+            }
+            else
+            {
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+}
