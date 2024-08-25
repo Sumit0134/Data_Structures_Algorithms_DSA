436b935ff92770c059d967c2efc7afb4b6a42680
Sumit Kumar Shaw <sumitshaw8274060134@gmail.com>
2024-08-25 18:57:20 +0530

    Arrays

---
+++ 2. Arrays/15. Left Rotation in an Array/main.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Array
+{
+    int size;
+    int length;
+    int *A;
+};
+
+void Create(struct Array *arr)
+{
+    printf("Enter the size of an array: ");
+    scanf("%d", &arr->size);
+    arr->A=(int *)malloc(arr->size*sizeof(int));
+    printf("Enter the length of an array: ");
+    scanf("%d", &arr->length);
+    if(arr->length>arr->size)
+    {
+        printf("Length is greater than the size.");
+        free(arr->A);
+        exit(1);
+    }
+    printf("Enter total of %d elements of an array: ", arr->length);
+    for(int i=0; i<arr->length; i++)
+    {
+        scanf("%d", &arr->A[i]);
+    }
+}
+
+void Display(struct Array arr)
+{
+    printf("The elements of an array is: ");
+    for(int i=0; i<arr.length; i++)
+    {
+        printf("%d ", arr.A[i]);
+    }
+    printf("\n");
+}
+
+void LeftRotation(struct Array *arr)
+{
+    int x=arr->A[0];
+    for(int i=1; i<arr->length; i++)
+    {
+        arr->A[i-1]=arr->A[i];
+    }
+    arr->A[arr->length-1]=x;
+}
+
+int main()
+{
+    struct Array arr;
+    Create(&arr);
+    Display(arr);
+    LeftRotation(&arr);
+    printf("After left rotation: ");
+    Display(arr);
+
+}
